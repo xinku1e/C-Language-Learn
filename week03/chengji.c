b8e9450129230ae85a5b483f968eafd1f8daf57a
xinku1e <40930416+xinku1e@users.noreply.github.com>
2018-07-09 22:50:54 +0800

    判断成绩是否及格

---
+++ week03/chengji.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+	const int PASS = 60;
+	int score;
+	
+	printf("请输入成绩：");
+	scanf("%d", &score);
+	
+	printf("你输入的成绩是：%d\n", score);
+	if(score < PASS)
+	    printf("很遗憾，这个成绩没有及格\n");
+	else
+	    printf("祝贺你，你及格了\n");
+	printf("再见！");
+	    
+	
+	return 0;
+ } 
