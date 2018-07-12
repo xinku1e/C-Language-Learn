8468f7ef314c10edd057e6c4f3cdf7c9aa7ced5a
xinku1e <40930416+xinku1e@users.noreply.github.com>
2018-07-12 18:00:20 +0800

    if 语句的常见错误

---
+++ week04/cuowu.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	double salary = 4000;
+	
+	int age = 55;
+	if(age>60);
+	{
+		salary = salary *1.2;
+		printf("%f",salary);
+	}
+	return 0;
+ } 
+ //if常见错误
+ //1.忘了大括号  2.if后面加分号 3.错误使用==和= 4.使人困惑的else 
