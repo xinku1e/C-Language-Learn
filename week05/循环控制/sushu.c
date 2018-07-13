4290c78e684402ee03465dac9f7e6003a6e2bfe2
xinkule <1756843904@qq.com>
2018-07-13 21:06:58 +0800

    判断素数

---
+++ week05/循环控制/sushu.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+	break; 跳出循环  continue跳过循环这一轮剩下的语句进入下一轮 
+	
+	int x;
+	scanf("%d",&x);
+	int i;
+	int isPrime = 1;
+	for(i=2;i<x;i++){
+		if(x%i==0){
+			isPrime = 0;
+			break;
+		}
+	}
+	if(isPrime==1){
+		printf("是素数");
+	}else{
+		printf("不是素数");
+	}
+	return 0;
+}
