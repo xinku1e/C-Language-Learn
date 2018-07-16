40d167086c422fc6a94e3cfc0f4a36ee22d04da6
xinkule <1756843904@qq.com>
2018-07-16 17:38:49 +0800

    打印n个素数  用数组做

---
+++ week07/01 数组运算/sushu06.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int isPrime(int x, int knownPrimes[], int numberOfKnownPrimes);
+
+int main()
+{
+	//´òÓ¡n¸öËØÊı 
+	const int number = 100;
+	int prime[number];
+	int i;
+	for(i=0; i<number; i++){
+		prime[i]=2;
+	}
+	int count = 1;
+	i = 3;
+	while (count < number){
+		if(isPrime(i, prime, count)){
+			prime[count++] = i;
+		}
+		i++;
+	}
+	for(i=0; i<count; i++){
+		printf("%d ", prime[i]);
+		if((i+1)%5==0) printf("\n");
+	}
+	printf("\n");
+	return 0;
+ } 
+ 
+ int isPrime(int x, int knownPrimes[], int numberOfKnownPrimes){
+ 	int ret = 1;
+ 	int i;
+ 	for(i=0; i<numberOfKnownPrimes; i++){
+ 		if(x%knownPrimes[i] == 0){
+ 			ret = 0;
+ 			break;
+		 }
+	 }
+	 return ret;
+ }
