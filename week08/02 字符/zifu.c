d872eb1e55503c3db7d6c84aeb9c901cc9fef84d
xinkule <1756843904@qq.com>
2018-07-17 17:10:18 +0800

    字符的输入输出

---
+++ week08/02 字符/zifu.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	char c;
+	int i;
+	char c1; 
+	c1 = i;
+	scanf("%c", &c);
+	scanf("%d", &i);
+	printf("c = %d\n", c);
+	printf("c = %c\n", c);
+	printf("c1 = %d\n", c1);
+	printf("c1 = %c\n", c1);
+	return 0;
+ } 
